803bcda40583ee9e639e59a451fe676ec408fdf9
Kejing <psr13848@students.ephec.be>
2026-05-22 15:55:14 +0200

    ça tourne plus qu'à modifier les call front pour aller chercher les bonnes infos

--- backend/src/main/java/be/angularpadelclub/PadelBackApplication.java
+++ backend/src/main/java/be/angularpadelclub/PadelBackApplication.java
@@ -1,13 +1,12 @@
-package be.angularpadelclub.padelback;
+package be.angularpadelclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "be.angularpadelclub")
 public class PadelBackApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PadelBackApplication.class, args);
     }
-
 }
