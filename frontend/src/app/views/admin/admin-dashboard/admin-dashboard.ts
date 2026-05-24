7ea0a55a5da01d21bd26e3ba9a3124b64517aaea
vince.h <v.harmegnies@gmail.com>
2026-05-24 12:38:36 +0200

    ajour admin global+site et couleurs différentes

--- frontend/src/app/views/admin/admin-dashboard/admin-dashboard.ts
+++ frontend/src/app/views/admin/admin-dashboard/admin-dashboard.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { AdminCardComponent } from '../admin-card/admin-card';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { AdminCardComponent } from '../admin-card/admin-card';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -9,4 +9,10 @@ import { RouterLink } from '@angular/router';
   imports: [AdminCardComponent, RouterLink, MatIconModule],
   templateUrl: './admin-dashboard.html'
 })
-export class AdminDashboard {}
+export class AdminDashboard {
+  adminType = signal<'GLOBAL' | 'SITE'>('SITE');
+
+  isGlobalAdmin(): boolean {
+    return this.adminType() === 'GLOBAL';
+  }
+}
