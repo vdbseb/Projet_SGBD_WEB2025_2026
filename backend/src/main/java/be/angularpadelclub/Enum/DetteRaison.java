299e0993e6e5de840d91c199a4c192309a7a0d57
Kejing <psr13848@students.ephec.be>
2026-05-28 23:25:19 +0200

    Ajoute gestion des paiements et dettes

---
+++ backend/src/main/java/be/angularpadelclub/Enum/DetteRaison.java
@@ -0,0 +1,7 @@
+package be.angularpadelclub.Enum;
+
+public enum DetteRaison {
+    PARTICIPATION_IMPAYEE,
+    SOLDE_ORGANISATEUR,
+    AJUSTEMENT
+}
---
+++ backend/src/main/java/be/angularpadelclub/Enum/ParticipationStatut.java
@@ -0,0 +1,8 @@
+package be.angularpadelclub.Enum;
+
+public enum ParticipationStatut {
+    EN_ATTENTE_PAIEMENT,
+    PAYEE,
+    LIBEREE,
+    ANNULEE
+}
